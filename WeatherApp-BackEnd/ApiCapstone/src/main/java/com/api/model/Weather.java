9425bc6bce9fd4acb8270ef81a2c9ac8b21b4698
Rohit Verma <vrohitcse@gmail.com>
2021-10-31 23:25:35 +0530

    Added few comments to make file more understandable for a naive user

--- WeatherApp-BackEnd/ApiCapstone/src/main/java/com/api/model/Weather.java
+++ WeatherApp-BackEnd/ApiCapstone/src/main/java/com/api/model/Weather.java
@@ -4,8 +4,9 @@ import java.util.HashMap;
 
 
 public class Weather {
-
+	// City variable for storing city's name.
 	private String city;
+	// Weather Details Map for storing the details city wise.
 	private HashMap<String,Object> weatherDetails =new HashMap<>();
 	public Weather(String city, HashMap<String, Object> weatherDetails) {
 		super();
